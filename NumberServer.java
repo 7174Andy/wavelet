6cf6845c09bd46fe9b0b4f2b6dfe5d68d8623bd7
7174Andy <hee031011@likelion.org>
2023-04-13 09:49:17 -0700

    search engine

--- NumberServer.java
+++ NumberServer.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
+    ArrayList<String> strings = new ArrayList<String>();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
@@ -19,6 +21,20 @@ class Handler implements URLHandler {
                 if (parameters[0].equals("count")) {
                     num += Integer.parseInt(parameters[1]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                } else if (parameters[0].equals("s")) {
+                    strings.add(parameters[1]);
+                    return String.format("String added!");
+                }
+            } else if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    String result = "";
+                    for (String s : strings) {
+                        if (s.contains(parameters[1])) {
+                            result += s + " ";
+                        }
+                    }
+                    return result;
                 }
             }
             return "404 Not Found!";
